378f1c64dca805dc7dede3445e093870da5de3a0
березка123 <berezkaopat@gmail.com>
2024-10-05 14:51:32 +0300

    training

---
+++ training/task_1_1.py
@@ -0,0 +1,28 @@
+# my_height = 170
+# print("Мой рост:", my_height, "см")
+
+# my_name = "Марина"
+# my_name = "Марина Землякова"
+# print("Меня зовут", my_name)
+
+# pet_name = input("Как зовут вашего питомца? ")
+# print("Ваш любимчик - " + pet_name)
+
+# def print_python():
+#    print("Учу Python!")
+
+
+# print_python()
+
+
+def print_letter(letter):
+    print(letter, end='')
+
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
